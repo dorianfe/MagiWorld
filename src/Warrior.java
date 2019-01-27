0c9c6767f77e454bed9da92997fbc2f634575ce6
dorianfe <fenestre.dorian@free.fr>
2019-01-27 15:01:27 +0100

    nv MagiWorld

---
+++ src/Warrior.java
@@ -0,0 +1,8 @@
+public class Warrior extends Hero {
+
+
+    public Warrior(int strength, int intelligence, int agility, int level, int health) {
+        super(strength, intelligence, agility, level, health);
+
+    }
+}
